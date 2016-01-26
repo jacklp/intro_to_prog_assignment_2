28939baff1b23bcf80bb67404c656b5aa04109d7
alejandroSaura <alejandro.saura.villanueva@gmail.com>
2016-01-26 21:41:57 +0000

    cookies WorkInProgress

---
+++ SlotMachineProject/Assets/CatMouth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatMouth : MonoBehaviour 
+{
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter () 
+	{
+		Debug.Log("Mouth detected cookie");
+	}
+
+	void OnTriggerExit () 
+	{
+		Debug.Log("Mouth lost cookie");
+	}
+
+	void OnTriggerStay () 
+	{
+		Debug.Log("Mouth attracting cookie");
+	}
+}
